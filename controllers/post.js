ae3a7fc97c7fa7c83c73c5e4483291724589fd8e
HtetTunLu <scm.htettunlu@gmail.com>
2023-09-04 16:22:40 +0630

    file upload, post db

--- controllers/post.js
+++ controllers/post.js
@@ -1,14 +1,30 @@
+const DB = require("../dbs/post");
+const Helper = require("../utils/helper");
+
 const all = async (req, res, next) => {
-  res.json({ msg: "All posts" });
+  // populate = join table && "-password didn't get password attribute"
+  let posts = await DB.find().populate("user", "-password -__v");
+  Helper.fMsg(res, "All posts", posts);
 };
 
 const get = async (req, res, next) => {
-  res.json({ msg: "one post" });
+  // populate = join table && "-password didn't get password attribute"
+  const post = await DB.findById(req.params.id).populate(
+    "user",
+    "-password -__v"
+  );
+  if (post) {
+    Helper.fMsg(res, "Single post", post);
+  } else {
+    next(new Error("No post with that id"));
+  }
 };
 
 const post = async (req, res, next) => {
-  res.json({ msg: "create" });
+  const result = await new DB(req.body).save();
+  Helper.fMsg(res, "Post added", result);
 };
+
 const patch = async (req, res, next) => {
   res.json({ msg: "edit" });
 };
--- server.js
+++ server.js
@@ -3,16 +3,51 @@ require("dotenv").config(); // access .env
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+const path = require("path");
 
 mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`);
 
 app.use(express.json()); // to get (req => body data)
+app.use(fileUpload());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const userRoute = require("./routes/user");
 const postRoute = require("./routes/post");
+const { saveFile, saveFiles, deleteFile } = require("./utils/gallery");
+
+// const funky = (req, res, next) => {
+//   res.json({ msg: "Comming with get method" });
+// };
+
+// const isLoggedIn = (req, res, next) => {
+//   if (1 + 1 == 2) {
+//     req.successMsg("We are good to go");
+//     next();
+//   } else {
+//     next(new Error("You are not logged in "));
+//   }
+// };
+
+// const isAdmin = (req, res, next) => {
+//   if (4 == 4) {
+//     console.log(req.successMsg);
+//     next();
+//   } else {
+//     next(new Error("Only admin can access this route"));
+//   }
+// };
+
+// app.get("/users", isLoggedIn, isAdmin, funky);
 
 app.use("/users", userRoute);
 app.use("/posts", postRoute);
+
+app.post("/gallery", saveFiles, async (req, res, next) => {
+  // await deleteFile(req.body.filename);
+  res.status(200).json({ msg: "File upload", filename: req.body.images });
+});
 
 app.use((err, req, res, next) => {
   err.status = err.status || 200;
